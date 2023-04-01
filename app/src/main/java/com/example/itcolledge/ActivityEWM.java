d28ad7d294de2e7151fe8b69c5fce1a3afc35f9a
okamodgi <nadakupnada@gmail.com>
2023-04-01 11:23:51 +0300

    3

--- app/src/main/java/com/example/itcolledge/ActivityEWM.java
+++ app/src/main/java/com/example/itcolledge/ActivityEWM.java
@@ -2,7 +2,9 @@ package com.example.itcolledge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ActivityEWM extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class ActivityEWM extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ewm);
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        TextView text = findViewById(R.id.txt);
+        text.setText(bundle.getString("Key"));
     }
 }
--- app/src/main/java/com/example/itcolledge/MainActivity.java
+++ app/src/main/java/com/example/itcolledge/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
                 "Численные методы", "Ин. яз.", "Системной программирование", "Стандартизация", "Тестирование"};
 
         ListView listView = findViewById(R.id.list);
-        TextView textView = findViewById(R.id.text);
+
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(
                 this,
@@ -32,10 +32,64 @@ public class MainActivity extends AppCompatActivity {
                 listOfSubjects
         );
         listView.setAdapter(adapter);
+
+        String ewm = "        Элементы\nвысшей математики";
+        String rmp = "         Разработка\n мобильных приложений";
+        String fzk = "Физическая культура";
+        String arc = "        Архитектура\n аппаратных средств";
+        String rpm = "        Разработка \nпрограммных модулей";
+        String dmt = "Дискретная математика";
+        String eco = "Экономика отрасли";
+        String chm = "Численные методы";
+        String eng = "Иностранный язык";
+        String ssp = "Системное программирование";
+        String std = "Стандартизация, сертификация,\n техническое документирование";
+        String tst = "Поддержка и тестирование";
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Intent intent = new Intent(MainActivity.this, ActivityEWM.class);
+
+                switch (i){
+                    case 0:
+                        intent.putExtra("Key", rmp);
+                        break;
+                    case 1:
+                        intent.putExtra("Key", ewm);
+                        break;
+                    case 2:
+                        intent.putExtra("Key", fzk);
+                        break;
+                    case 3:
+                        intent.putExtra("Key", arc);
+                        break;
+                    case 4:
+                        intent.putExtra("Key", rpm);
+                        break;
+                    case 5:
+                        intent.putExtra("Key", dmt);
+                        break;
+                    case 6:
+                        intent.putExtra("Key", eco);
+                        break;
+                    case 7:
+                        intent.putExtra("Key", chm);
+                        break;
+                    case 8:
+                        intent.putExtra("Key", eng);
+                        break;
+                    case 9:
+                        intent.putExtra("Key", ssp);
+                        break;
+                    case 10:
+                        intent.putExtra("Key", std);
+                        break;
+                    case 11:
+                        intent.putExtra("Key", tst);
+                        break;
+
+                }
                 startActivity(intent);
             }
         });
